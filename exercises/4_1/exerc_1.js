10f81687b1509052a9b00549578bb1c8935e05cf
William Dieter <williamzdieter@gmail.com>
2020-07-15 18:20:14 -0300

    Exercicio 3

---
+++ exercises/4_1/exerc_1.js
@@ -0,0 +1,15 @@
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis a e b definidas no começo com os valores que serão operados. Faça programas para:
+//Adição
+//Subtracão
+//Multiplicação
+//Divisão
+//Módulo
+
+var a = 10;
+var b = 5;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
---
+++ exercises/4_1/exerc_2.js
@@ -0,0 +1,10 @@
+//Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas variáveis com os valores que serão comparados.
+
+var a = 20;
+var b = 21;
+
+if (a > b) {
+  console.log(''a' é maior que 'b'');
+} else {
+  console.log(''b' é maior que 'a'');
+};
