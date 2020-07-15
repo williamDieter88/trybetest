0e69666fffefab7fd53f1fba2e11f7416708306f
William Dieter <williamzdieter@gmail.com>
2020-07-14 23:15:34 -0300

    Exercicio bonus 2

---
+++ exercises/4_2/exerc_7.js
@@ -0,0 +1,14 @@
+//Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável smallestNumber poderia ser qualquer um, desde que fosse maior que o maior número do array numbers. Caso atribuíssemos o valor 1 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número menor que 1 no array.
+
+let smallestNumber = 1000;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < smallestNumber) {
+    smallestNumber = numbers[i];
+  }
+}
+
+console.log(smallestNumber);
