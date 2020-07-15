f329013d0900a7d44ed0f4930db5dc8b44584cda
William Dieter <williamzdieter@gmail.com>
2020-07-15 17:56:13 -0300

    Exercicio 3

---
+++ exercises/4_3/.vscode/exerc_3.js
@@ -0,0 +1,21 @@
+//Agora inverta o lado do triângulo.
+
+let n = 5;
+let lineIndex;
+let columnIndex;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for (lineIndex = 0; lineIndex < n; lineIndex++) {
+  for (columnIndex = 0; columnIndex <= n; columnIndex++) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition--;
+};
