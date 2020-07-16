b3623993d1d22feb1702ac33f5e60f1c850567a9
William Dieter <williamzdieter@gmail.com>
2020-07-16 19:15:09 -0300

    Exercicio 1 parte 3

--- exercises/4_4/exerc_parte2_1.js
+++ exercises/4_4/exerc_parte2_1.js
@@ -1,6 +1,10 @@
 //Agora vamos fazer um exercício que vai deixar claro como funções com responsabilidades bem definidas deixam o código mais bem escrito.
 //Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
-
+//Exemplo de palíndromo: arara.
+//verificaPalindrome("arara");
+//Retorno esperado: true
+//verificaPalindrome("desenvolvimento");
+//Retorno esperado: false
 function verificaPalindrome(palavra) {
     let arrayLetras = palavra.split("");
     let isPalindrome = true;
