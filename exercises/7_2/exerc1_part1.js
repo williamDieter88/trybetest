aabecb338fe71e312c75969da1bc00e5abd25422
William Dieter <williamzdieter@gmail.com>
2020-08-12 19:27:30 -0300

    Exercicio 1 parte 1

---
+++ exercises/7_2/exerc1_part1.js
@@ -0,0 +1,11 @@
+// Use the playerNumber variable to look up player 16 in testObj using bracket notation. Then assign that name to the player variable.
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line;
+  
+  var playerNumber = 16; // Change this Line
+  var player = testObj[playerNumber]; // Change this Line
