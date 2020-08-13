b8e60795807df9efe9b6f639ee0c8607f3fbb77c
William Dieter <williamzdieter@gmail.com>
2020-08-13 02:42:33 -0300

    Exercicio 3 parte 2

---
+++ exercises/7_2/exerc3_part2.js
@@ -0,0 +1,22 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+  //Crie uma função para mostrar o tamanho de um objeto.
+  const sizeObj = (obj) => Object.keys(obj).length;
+  console.log(sizeObj(lesson2));
