b47f57ea8a1ee75588511fdafda8d0b93cd5e4b2
William Dieter <williamzdieter@gmail.com>
2020-08-13 02:45:09 -0300

    Exercicio 4 parte 2

---
+++ exercises/7_2/exerc4_part2.js
@@ -0,0 +1,22 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+  //Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+  const listValues = (obj) =>  Object.values(obj);
+  console.log(listValues(lesson1));
