847c6ff49353c931d0151212755f44f61f2ffb5c
William Dieter <williamzdieter@gmail.com>
2020-09-03 15:40:22 -0300

    Add exercicio myIndexOf

---
+++ exercises/10_1/myIndexOf.test.js
@@ -0,0 +1,14 @@
+const myIndexOf = require('./myIndexOf');
+const { TestScheduler } = require('jest');
+
+describe('myIndexOf', () => {
+  const a = [1, 2, 3, 4];
+
+  test('returns the item index when the array contains the item', () => {
+    expect(myIndexOf(a, 3)).toBe(2);
+  });
+
+  test('return -1 if the array does not contain the item', () => {
+    expect(myIndexOf(a, 5)).toBe(-1);
+  });
+});
